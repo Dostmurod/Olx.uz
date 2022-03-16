6773faea842ffef2d88a9e23a029703815d39a1e
Dostmurod <dostmurodzoirov777@gmail.com>
2022-03-16 20:23:48 +0500

    scrollProgress

--- script.js
+++ script.js
@@ -132,7 +132,13 @@ window.addEventListener("scroll", function(){
         lastScrollTop = scrollTop
 })
 
+const scrollProgress = document.getElementById('scrollbarProgress');
+const heightDoc = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
+window.addEventListener('scroll', () => {
+    const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    scrollProgress.style.width = `${(scrollTop / heightDoc) * 100}%`
+})
 
 
 
